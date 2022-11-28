e57e6f9fe750ca0b63d1d78796c270b037ad7b92
Professora Tábata Macedo <106038097+TabMacedo1897@users.noreply.github.com>
2022-11-28 12:09:10 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+var canvas = new fabric.Canvas('myCanvas')
+playerX = 10
+playerY = 10
+
+blockImageWidth = 30
+blockImageHeight = 30
+
+var playerObject = ""
+var blockImageObject = ""
+
+function playerUpdate(){
+    fabric.Image.fromURL("player.png", function(Img){
+      playerObject = Img
+
+      playerObject.scaleToWidth(150)
+      playerObject.scaleToHeight(140)
+
+      playerObject.set({
+        top: playerY,
+        left:playerX
+      })
+      canvas.add(playerObject)
+    })
+}
+
+
+
+function newImage(getImage)
+{
+	fabric.Image.fromURL(getImage, function(Img) {
+	blockImageObject = Img;
+
+	blockImageObject.scaleToWidth(blockImageWidth);
+	blockImageObject.scaleToHeight(blockImageHeight);
+	blockImageObject.set({
+	top:playerY,
+	left:playerX
+	});
+	canvas.add(blockImageObject);
+
+	});
+
+}
+
+
